e2cd864569e534de364714c57c04495d742d2d2a
Luis Chamosa <85203820+luischamosa@users.noreply.github.com>
2025-05-18 22:50:19 -0600

    Update Program.cs
    Code improvement

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,11 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebApi
 {
@@ -13,14 +7,25 @@ namespace WebApi
     {
         public static void Main(string[] args)
         {
+            // Build and run the host using the configured host builder
             CreateHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        /// Configures and returns the IHostBuilder for the application.
+        /// </summary>
+        /// <param name="args">Command-line arguments.</param>
+        /// <returns>IHostBuilder instance.</returns>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    // Specify the Startup class to use
                     webBuilder.UseStartup<Startup>();
+
+                    // Optionally, you can add more configurations here, for example:
+                    // webBuilder.UseUrls("http://localhost:5000");
+                    // webBuilder.CaptureStartupErrors(true);
                 });
     }
 }
